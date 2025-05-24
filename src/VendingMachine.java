d8e440222ebbefaa1a1b5dbf02d6363a93bea4b4
zplay80 <husniddin.mahkamov880@gmail.com>
2025-05-24 16:55:36 +0500

    changed

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,34 +1,74 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class VendingMachine {
+    private Map<String, Double> beverages = new HashMap<>();
+    private Map<Integer, Double> cards = new HashMap<>();
+    private Column[] columns = new Column[4];
 
     public VendingMachine() {
-
+        for (int i = 0; i < 4; i++) {
+            columns[i] = new Column();
+        }
     }
 
     public void addBeverage(String name, double price) {
-
+        beverages.put(name, price);
     }
 
     public double getPrice(String beverageName) {
-        return 0.0;
+        return beverages.getOrDefault(beverageName, -1.0);
     }
 
     public void rechargeCard(int cardId, double credit) {
-
+        cards.put(cardId, cards.getOrDefault(cardId, 0.0) + credit);
     }
 
     public double getCredit(int cardId) {
-        return 0.0;
+        return cards.getOrDefault(cardId, -1.0);
     }
 
     public void refillColumn(int column, String beverageName, int cans) {
-
+        int index = column - 1;
+        columns[index].beverageName = beverageName;
+        columns[index].cans = cans;
     }
 
     public int availableCans(String beverageName) {
-        return 0;
+        int total = 0;
+        for (Column col : columns) {
+            if (col.beverageName != null && col.beverageName.equals(beverageName)) {
+                total += col.cans;
+            }
+        }
+        return total;
     }
 
     public int sell(String beverageName, int cardId) {
-        return 0;
+        double price = getPrice(beverageName);
+        if (price == -1.0) {
+            return -1;
+        }
+        if (availableCans(beverageName) == 0) {
+            return -1;
+        }
+        double credit = getCredit(cardId);
+        if (credit < price) {
+            return -1;
+        }
+        for (int i = 0; i < 4; i++) {
+            Column col = columns[i];
+            if (col.beverageName != null && col.beverageName.equals(beverageName) && col.cans > 0) {
+                col.cans--;
+                cards.put(cardId, credit - price);
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+
+    private static class Column {
+        String beverageName;
+        int cans;
     }
 }
--- src/app.java
+++ src/app.java
@@ -1,32 +1,22 @@
 public class app {
     public static void main(String[] args) {
-
         VendingMachine vm = new VendingMachine();
-
         vm.addBeverage("Coke", 0.50);
         vm.addBeverage("Water", 0.30);
         vm.addBeverage("Beer", 1.00);
-
         vm.refillColumn(1, "Coke", 1);
         vm.refillColumn(2, "Beer", 10);
         vm.refillColumn(3, "Coke", 15);
         vm.refillColumn(4, "Water", 20);
-
         System.out.println(vm.availableCans("Coke"));
-
         System.out.println(vm.getPrice("Coke"));
-
         vm.rechargeCard(12, 5.5);
         vm.rechargeCard(21, 10.0);
         vm.rechargeCard(99, 0.75);
-
         vm.sell("Coke", 12);
         vm.sell("Coke", 99);
         vm.sell("Water", 12);
-
         System.out.println(vm.availableCans("Coke"));
-
         System.out.println(vm.getCredit(12));
-
     }
 }
